0cec468e6d68dda4b299e7a59ffd7805ccea126f
Brendon Kuleck <brendonkuleck@gmail.com>
2017-05-07 16:10:36 -0700

    Automated tweeting

---
+++ index.js
@@ -0,0 +1,17 @@
+var Twit = require('twit');
+
+var bot = new Twit({
+  consumer_key: process.env.TWITTERBOT_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTERBOT_CONSUMER_SECRET,
+  access_token: process.env.TWITTERBOT_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTERBOT_ACCESS_TOKEN_SECRET,
+  timeout_ms: 60*1000
+});
+
+bot.post('statuses/update', {status: 'ok just gonna have robuts tweet for me from now on'}, function (err, data, response){
+  if(err){
+    console.log(err);
+  } else {
+    console.log(data.text + ' was tweeted.');
+  }
+})
